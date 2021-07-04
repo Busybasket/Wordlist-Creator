8590a2fc8144952a6623e135132c877515917ba7
Busybasket <86014728+Busybasket@users.noreply.github.com>
2021-07-04 14:16:17 +0300

    Add files via upload

---
+++ WordList.py
@@ -0,0 +1,29 @@
+# word list türkçe
+import random
+txt = 0
+values = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","0","1","2","3","4","5","6","7","8","9",]
+txtValues = []
+setList = set()
+dosya = open("WordList.txt","a",encoding="utf-8")
+
+for i in range(1,3010936484):
+    d = i%1000000
+    if d == 0:
+        a = len(txtValues)
+        setList.update()
+        for i in range(0,a):
+            dosya.write(txtValues[i] + "\n")
+        dosya.close
+        dosya = open("WordList.txt","a",encoding="utf-8")
+        txtValues.clear()
+    else:
+        a = random.choice(values)
+        b = random.choice(values)
+        c = random.choice(values)
+        d = random.choice(values)
+        e = random.choice(values)
+        f = random.choice(values)
+        lastValue = a+b+c+d+e+f
+        txtValues.append(lastValue)
+setList.update(txtValues)
+
