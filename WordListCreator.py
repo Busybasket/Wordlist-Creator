c550c1cbb3b2b7ef1bf9fea5f45534e9f2c3c605
04L <79569666+04L@users.noreply.github.com>
2021-07-31 12:21:54 +0300

    Code fix & value addition & guide writing
    Removing the unused code in the code, adding special characters for the wordlist and writing the user guide.

--- WordListCreator.py
+++ WordListCreator.py
@@ -1,7 +1,7 @@
 # word list türkçe
 import random
-txt = 0
-values = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","0","1","2","3","4","5","6","7","8","9",]
+
+values = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","r","s","t","u","v","y","z","0","1","2","3","4","5","6","7","8","9","%","$","@","'",";","?","*","-","_",".","."]
 txtValues = []
 setList = set()
 dosya = open("WordList.txt","a",encoding="utf-8")
